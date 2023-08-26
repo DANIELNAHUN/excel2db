a44522b58504da6c77cdf547fc37e7562dae268f
DANIELNAHUN <nahuncf5@gmail.com>
2023-08-26 09:46:58 -0500

    update bd

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
+import os
+
 import pandas as pd
+import pymysql
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def connection_db_local():
+    conn = pymysql.connect(host=os.getenv("IP_SERVER_DATABASE"), database=os.getenv(
+        'BD_SERVER'), user=os.getenv('USER_SERVER'), passwd=os.getenv('PASSWORD_SERVER'))
+    return conn
 
 
 def read_excel(file):
     df = pd.read_excel(file)
-    rows, columns = df.shape
-    result = list()
-    for i in range(0, rows):
-        values =[]
-        for j in range(0,columns):
-            val = df.loc[i][j]
-            if val != val:
-                val = ""
-            values.append(val)
-        tpl = tuple(values)
-        result.append(tpl)
-    with open("files\df.txt",'w')as file:
-        file.write(str(result))
+    for i, row in df.iterrows():
+        tarifa = row['tarifa_internet']
+        velocidad = int(row['velocidad'])
+        sql = f"UPDATE dim_tarifas_internet SET velocidad = {velocidad} WHERE tarifa_internet LIKE '{tarifa}%'"
+        print(sql)
+    #     values =[]
+    #     for j in range(0,columns):
+    #         val = df.loc[i][j]
+    #         if val != val:
+    #             val = ""
+    #         values.append(val)
+    #     tpl = tuple(values)
+    #     result.append(tpl)
+    # with open("files\df.txt",'w')as file:
+    #     file.write(str(result))
 
 
-url= "files\Tabla de abonos.xlsx"
+url= "files\Tarifa_internet.xlsx"
 read_excel(url)
