267ad867a238907815b0082c595d127bab0c4089
DANIELNAHUN <nahuncf5@gmail.com>
2023-08-30 18:06:28 -0500

    df to list

--- main.py
+++ main.py
@@ -14,22 +14,26 @@ def connection_db_local():
 
 def read_excel(file):
     df = pd.read_excel(file)
-    for i, row in df.iterrows():
-        tarifa = row['tarifa_internet']
-        velocidad = int(row['velocidad'])
-        sql = f"UPDATE dim_tarifas_internet SET velocidad = {velocidad} WHERE tarifa_internet LIKE '{tarifa}'"
+    list_df = df.to_records(index=False)
+    list_df = list(list_df)
+    with open('files\df_prod_nom.txt','w') as txt:
+        txt.write(str(list_df))
+    # for i, row in df.iterrows():
+    #     tarifa = row['tarifa_internet']
+    #     velocidad = int(row['velocidad'])
+    #     sql = f"UPDATE dim_tarifas_internet SET velocidad = {velocidad} WHERE tarifa_internet LIKE '{tarifa}'"
 
-        try:
-            conn = connection_db_local()
-            try:
-                with conn.cursor() as cur:
-                    cur.execute(sql)
-                    conn.commit()
-            finally:
-                conn.close()
-        except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
-            print(f"Error not connect in mysql {e}")
+    #     try:
+    #         conn = connection_db_local()
+    #         try:
+    #             with conn.cursor() as cur:
+    #                 cur.execute(sql)
+    #                 conn.commit()
+    #         finally:
+    #             conn.close()
+    #     except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
+    #         print(f"Error not connect in mysql {e}")
 
 
-url= "files\Tarifa_internet.xlsx"
+url= "files\productos_nomina.xlsx"
 read_excel(url)
